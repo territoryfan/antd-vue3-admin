063a831a88aacf4b8ca29caf44d7335c6045f59a
“territory” <territoryfan@163.com>
2022-09-22 23:22:59 +0800

    feat: delete

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <ConfigProvider :locale="getAntdLocale">
-    <AppProvider>
-      <RouterView />
-    </AppProvider>
-  </ConfigProvider>
+  <div>
+    sfsdfsdfdsf
+    <HelloWorld />
+  </div>
 </template>
 
 <script lang="ts" setup>
-  import { ConfigProvider } from 'ant-design-vue';
-  import { AppProvider } from '/@/components/Application';
-  import { useTitle } from '/@/hooks/web/useTitle';
-  import { useLocale } from '/@/locales/useLocale';
+  import HelloWorld from './views/HelloWorld.vue';
+  // import { ConfigProvider } from 'ant-design-vue';
+  // import { AppProvider } from '/@/components/Application';
+  // import { useTitle } from '/@/hooks/web/useTitle';
+  // import { useLocale } from '/@/locales/useLocale';
 
-  import 'dayjs/locale/zh-cn';
-  // support Multi-language
-  const { getAntdLocale } = useLocale();
+  // import 'dayjs/locale/zh-cn';
+  // // support Multi-language
+  // const { getAntdLocale } = useLocale();
 
-  // Listening to page changes and dynamically changing site titles
-  useTitle();
+  // // Listening to page changes and dynamically changing site titles
+  // useTitle();
 </script>
